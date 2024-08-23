69769edf2082d6179d448ce04d37454574bd0d6c
robertlennox <robertlennox9@gmail.com>
2024-08-23 11:20:37 +0200

    saving dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,14 +1,17 @@
 ## code to prepare `DATASET` dataset goes here
 
 setwd("C:/Users/rb501745/OneDrive - Dalhousie University/lennox/data-raw")
-cb_rivers<-readRDS("cb_rivers.RDS")
 margaree<-readRDS("margaree.RDS")
 stewi<-readRDS("stewi.RDS")
 wallace<-readRDS("wallace.RDS")
+rivers<-readRDS("rivers.RDS")
+ns<-readRDS("ns.RDS")
 
-usethis::use_data(cb_rivers, overwrite = TRUE)
+
 usethis::use_data(margaree, overwrite = TRUE)
 usethis::use_data(stewi, overwrite = TRUE)
 usethis::use_data(wallace, overwrite = TRUE)
+usethis::use_data(ns, overwrite = TRUE)
+usethis::use_data(rivers, overwrite = TRUE)
 
 
