449042ac5318d0e082dc65ff636f78b1485a892f
robertlennox <42450990+robertlennox@users.noreply.github.com>
2026-09-10 18:00:53 -0300

    fixing shapes

---
+++ R/data.R
@@ -0,0 +1,107 @@
+#' Margaree watershed
+#'
+#' Spatial data for the Margaree watershed.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"margaree"
+
+#' Stewiacke watershed
+#'
+#' Spatial data for the Stewiacke watershed.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"stewi"
+
+#' Wallace watershed
+#'
+#' Spatial data for the Wallace watershed.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"wallace"
+
+#' Nova Scotia rivers
+#'
+#' Spatial river data.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"rivers"
+
+#' Nova Scotia boundary
+#'
+#' Nova Scotia boundary polygon.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"ns"
+
+#' Scotian Shelf
+#'
+#' Scotian Shelf spatial data.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"scotian"
+
+#' Labrador Sea
+#'
+#' Labrador Sea spatial data.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"lab"
+
+#' Marine Institute locations
+#'
+#' Spatial data imported from KML.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"mil"
+
+#' Nova Scotia watersheds
+#'
+#' Nova Scotia watershed boundaries.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"nshires"
+
+#' Inverness Harbour
+#'
+#' Inverness Harbour spatial data.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"inverness"
+
+#' Cheticamp Harbour
+#'
+#' Cheticamp Harbour spatial data.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"cheticamp"
+
+#' Cape Breton rivers
+#'
+#' Cape Breton river spatial data.
+#'
+#' @format An sf object.
+#' @keywords datasets
+#' @export
+"cb_rivers"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,34 +1,24 @@
-## code to prepare `DATASET` dataset goes here
+## data-raw/DATASET.R
+##
+## Script to prepare package datasets
 
-setwd("C:/Users/rb501745/OneDrive - Dalhousie University/lennox/data-raw")
-margaree<-readRDS("margaree.RDS")
-stewi<-readRDS("stewi.RDS")
-wallace<-readRDS("wallace.RDS")
-rivers<-readRDS("rivers.RDS")
-ns<-readRDS("ns.RDS")
-scotian<-readRDS("scotian.RDS")
-lab<-readRDS("labsea.RDS")
-mil<-sf::st_read("mil.kml")
-nshires<-readRDS("nshires.RDS")
-inverness <- readRDS(here::here('data-raw', 'inverness_harbour.rds'))
-cheticamp <- readRDS(here::here('data-raw', 'cheticamp_harbour.rds'))
+library(sf)
+library(usethis)
+library(here)
 
+# Load datasets -----------------------------------------------------------
 
+margaree <- readRDS(here("data-raw", "margaree.RDS"))
+stewi <- readRDS(here("data-raw", "stewi.RDS"))
+wallace <- readRDS(here("data-raw", "wallace.RDS"))
+rivers <- readRDS(here("data-raw", "rivers.RDS"))
+ns <- readRDS(here("data-raw", "ns.RDS"))
+scotian <- readRDS(here("data-raw", "scotian.RDS"))
+lab <- readRDS(here("data-raw", "labsea.RDS"))
+nshires <- readRDS(here("data-raw", "nshires.RDS"))
 
+inverness <- readRDS(
+  here("data-raw", "inverness_harbour.rds")
+)
 
-usethis::use_data(margaree, overwrite = TRUE)
-usethis::use_data(stewi, overwrite = TRUE)
-usethis::use_data(wallace, overwrite = TRUE)
-usethis::use_data(ns, overwrite = TRUE)
-usethis::use_data(rivers, overwrite = TRUE)
-usethis::use_data(scotian, overwrite = TRUE)
-usethis::use_data(mil, overwrite = TRUE)
-usethis::use_data(lab, overwrite = TRUE)
-usethis::use_data(nshires, overwrite = TRUE)
-usethis::use_data(inverness, overwrite = TRUE)
-usethis::use_data(cheticamp, overwrite = TRUE)
-
-
-
-
-
+cheticamp <- readRDS(here("data-raw", "cheticamp_harbour.rds"))
